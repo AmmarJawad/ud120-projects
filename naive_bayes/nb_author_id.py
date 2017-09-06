888dc711866b32918ac7838a7bb36d65bc77fdc0
Ammar Jawad <b-ajawad@hotmail.com>
2017-09-06 12:15:08 +0100

    Completed naive bayes gaussian classifier for Sara/Chris label prediction

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -29,5 +29,12 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-
-
+def naive_classifier(features_train, labels_train):
+    from sklearn.naive_bayes import GaussianNB
+    clf = GaussianNB()
+    clf_fit = clf.fit(features_train, labels_train)
+    clf_predict = clf.predict(features_test)
+    from sklearn.metrics import accuracy_score
+    acc = accuracy_score(pred, labels_test)
+    return acc
+naive_classifier(features_train, labels_train)
