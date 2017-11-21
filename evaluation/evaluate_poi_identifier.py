c2577a9e504953fca91f1bd13efd4484cfa42214
Ammar Jawad Doosh <ajawad@radiantworlds.com>
2017-11-21 14:16:22 +0000

    Completed Lesson 14, 15, and 16. All that remains is the final project.

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 
 
 """
@@ -15,6 +14,11 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -26,6 +30,21 @@ labels, features = targetFeatureSplit(data)
 
 
 
-### your code goes here 
+### your code goes here
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print accuracy_score(pred, labels_test)
 
 
+counter = 0
+for label in pred:
+    if label == 1.:
+        counter += 1
+print "Pois predicted: ", counter
+print "Total people in test set: ", len(labels_test)
+print "If identifier predicted 0.0 (not POI) for everyone in the test set, what would the accuracy be?", (float(len(labels_test))-float(counter))/float(len(labels_test))
+print "precision_score: ", precision_score(pred, labels_test)
+print "recall_score: ", recall_score(pred, labels_test)
